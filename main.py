5a472c87d073e79e83c76bde2a7ee235f5dbac46
mqasim41 <mqasim.bese21seecs@seecs.edu.pk>
2024-07-14 22:14:09 +0500

    FIxes

--- main.py
+++ main.py
@@ -7,6 +7,30 @@ from licensePlateReader.pipeline.stage_015_data_annotation import DataAnnotation
 
 
 
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Annotation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataAnnotationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
 
 STAGE_NAME = "Prepare base model"
 try: 
